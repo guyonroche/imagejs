108bcdc889c7ecf04c86311c94e6d93b067b0430
guyonroche <cyber.sapiens@hotmail.com>
2015-02-25 09:03:31 +0000

    Added bilinear interpolation resize

--- test/resize.js
+++ test/resize.js
@@ -4,11 +4,12 @@ var Bitmap = BitmapJS.Bitmap;
 var filename = process.argv[2];
 var width = parseInt(process.argv[3]);
 var height = parseInt(process.argv[4]);
+var algorithm = process.argv[5] || "nearestNeighbor";
 
 var bm = new Bitmap();
 bm.readFile(filename, BitmapJS.ImageType.JPG)
     .then(function() {
-        var bm2 = bm.resize({width: width, height: height, algorithm: "nearestNeighbor"});
+        var bm2 = bm.resize({width: width, height: height, algorithm: algorithm});
         var filename2 = filename.replace('.jpg', '.' + width + 'x' + height + '.jpg');
         return bm2.writeFile(filename2, BitmapJS.ImageType.JPG);
     })
