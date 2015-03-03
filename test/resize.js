5861a241379bc714e2c29c47e4807d5b9e608729
guyonroche <cyber.sapiens@hotmail.com>
2015-03-03 22:01:48 +0000

    Refactor - pad, crop removed from resize code

--- test/resize.js
+++ test/resize.js
@@ -1,13 +1,20 @@
 var BitmapJS = require("../index");
 var Bitmap = BitmapJS.Bitmap;
 
+if (process.argv.length < 5) {
+    console.log("Usage: node test/resize filename width height [algorithm] [fit]");
+    console.log("   algorithm: one of nearestNeighbour, bilinearInterpolation, bicubicInterpolation, bezierInterpolation");
+    console.log("   fit: one of pad, crop, stretch");
+    process.exit(0);
+}
+
 var filename = process.argv[2];
 var width = parseInt(process.argv[3]);
 var height = parseInt(process.argv[4]);
 var algorithm = process.argv[5] || "nearestNeighbor";
 var fit = process.argv[6] || "stretch";
 var bm = new Bitmap();
-var red = {r:255,g:255,b:0,a:255};
+var red = {r:255,g:0,b:0,a:255};
 bm.readFile(filename)
     .then(function() {
         var bm2 = bm.resize({width: width, height: height, algorithm: algorithm, fit: fit, padColor: red});
